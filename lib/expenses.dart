2f4a1f19570239c96b8f0f237d60d83df8fb9e8a
Joao Gabriel <jgafarias1@gmail.com>
2025-04-23 10:16:49 -0300

    Update

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -12,6 +12,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return const Text('The expense tracker!');
+    return Scaffold(
+      body: Column(
+        children: const [
+         Text('The cart'),
+         Text('Expenses List...'),
+        ],
+      ),
+    );
   }
 }
