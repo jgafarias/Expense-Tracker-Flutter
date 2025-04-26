ca864465f82eb0e002a56419e6f6fa276f805c93
Joao Gabriel <jgafarias1@gmail.com>
2025-04-26 13:37:01 -0300

    Update new_expense.dart

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -41,7 +41,12 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
-              TextButton(onPressed: () {}, child: const Text('Cancel')),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
