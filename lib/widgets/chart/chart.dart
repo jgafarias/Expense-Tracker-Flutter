c4b29eac7086d170b30005f4c98da04c826ec8b1
Joao Gabriel <jgafarias1@gmail.com>
2025-04-29 09:42:54 -0300

    Update

--- lib/widgets/chart/chart.dart
+++ lib/widgets/chart/chart.dart
@@ -35,18 +35,15 @@ class Chart extends StatelessWidget {
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Container(
       margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 8,
-      ),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
       width: double.infinity,
       height: 180,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         gradient: LinearGradient(
           colors: [
-            Theme.of(context).colorScheme.primary.withOpacity(0.3),
-            Theme.of(context).colorScheme.primary.withOpacity(0.0)
+            Theme.of(context).colorScheme.primary.withAlpha(70),
+            Theme.of(context).colorScheme.primary.withAlpha(10),
           ],
           begin: Alignment.bottomCenter,
           end: Alignment.topCenter,
@@ -60,34 +57,36 @@ class Chart extends StatelessWidget {
               children: [
                 for (final bucket in buckets) // alternative to map()
                   ChartBar(
-                    fill: bucket.totalExpenses == 0
-                        ? 0
-                        : bucket.totalExpenses / maxTotalExpense,
-                  )
+                    fill:
+                        bucket.totalExpenses == 0
+                            ? 0
+                            : bucket.totalExpenses / maxTotalExpense,
+                  ),
               ],
             ),
           ),
           const SizedBox(height: 12),
           Row(
-            children: buckets
-                .map(
-                  (bucket) => Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 4),
-                      child: Icon(
-                        categoryIcons[bucket.category],
-                        color: isDarkMode
-                            ? Theme.of(context).colorScheme.secondary
-                            : Theme.of(context)
-                                .colorScheme
-                                .primary
-                                .withOpacity(0.7),
+            children:
+                buckets
+                    .map(
+                      (bucket) => Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          child: Icon(
+                            categoryIcons[bucket.category],
+                            color:
+                                isDarkMode
+                                    ? Theme.of(context).colorScheme.secondary
+                                    : Theme.of(
+                                      context,
+                                    ).colorScheme.primary.withOpacity(0.7),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                )
-                .toList(),
-          )
+                    )
+                    .toList(),
+          ),
         ],
       ),
     );
